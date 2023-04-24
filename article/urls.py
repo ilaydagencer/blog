eefb176cc123271395ae45be71cf8835d1772c8f
ilaydagencer <120780410+ilaydagencer@users.noreply.github.com>
2023-04-24 18:00:19 +0300

    blog

---
+++ article/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+app_name="article"
+urlpatterns = [
+    path('dashboard/',views.dashboard,name="dashboard"),
+    path('addarticle/',views.addArticle,name="addarticle"),
+    path('article/<int:id>',views.detail,name="detail"),
+]
